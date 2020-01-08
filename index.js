081932cdfd3eb45e24521efa6b19365c7b9db4df
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-08 11:54:24 -0500

    server ready

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//const Users = require('./routes/Users');
+
+//app.use('/users', Users);
+
+app.listen(port, () => {
+    console.log(`Server on port:`, port);
+});
