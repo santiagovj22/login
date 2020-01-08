2b514bc2769a205896730eb2bbcb76c345b533bf
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-08 17:31:11 -0500

    add navbar and routes

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { Component} from 'react';
+import { Link, withRouter} from 'react-router-dom'
+
+class Navbar extends Component {
+    
+            logOut(e){
+                e.preventDefault()
+                localStorage.removeItem('usertoken')
+                this.props.history.push('/');
+            }
+
+            render(){
+                const loginRegLink = (
+                    <ul className = "navbar-nav">
+                        <li className = "nav-item">
+                            <Link to = "/login" className = "nav-link">Login</Link>
+                        </li>
+                        <li className = "nav-item">
+                            <Link to = "/register" className = "nav-link">Register</Link>
+                        </li>
+
+
+                    </ul>
+                )
+                const userLink = (
+                    <ul className = "navbar-nav">
+                        <li className = "nav-item">
+                            <Link to = "/profile" className = "nav-link">User</Link>
+                        </li>
+                        <li className = "nav-item">
+                           <a href = "" onClick ={this.logOut.bind(this)}  className = "nav-link">
+                           Logout
+                           </a>
+                        </li>
+
+                    </ul>
+                )
+                return(
+                    <nav className="navbar navbar-expand-lg-navbar-dark bg-dark rounded">
+                        <button className="navbar-toggler" type = "button">
+
+                        </button>
+                    </nav>
+                )
+            }
+}
+
+export default Navbar;
