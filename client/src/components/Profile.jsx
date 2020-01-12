1c84db7f69a56e15d9997bab47e0060b60881154
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-12 18:31:57 -0500

    front ready

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -20,4 +20,34 @@ class Profile extends Component {
             email: decoded.email
         })
     }
+    render() {
+        return(
+            <div className = "container">
+                <div className="jumbotron mt-5">
+                    <div className = "col-sm-8 mx-auto">
+                        <h1 className="text-center"> Profile</h1>
+                    </div>
+                    <table className="table col-md-6 mx-auto">
+                        <tbody>
+                            <tr>
+                                <td>First Name</td>
+                                <td>{this.state.first_name}</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>{this.state.last_name}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{this.state.email}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        )
+    }
 }
+
+export default Profile;
